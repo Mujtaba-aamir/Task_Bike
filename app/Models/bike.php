9ad19e71991f5cf9ef5c0d5883bfda0a52c03465
Mujtaba-aamir <ma7820464@gmail.com>
2025-06-25 15:20:06 +0500

    Revised Task 1

--- app/Models/bike.php
+++ app/Models/bike.php
@@ -6,13 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class bike extends Model
 {
-    protected $fillable = [
-        'plate_number',
-        'model',
-        'model_year',
-        'chassis_number',
-        'engine_number',
-        'mulkiya_front_image',
-        'mulkiya_back_image',
-    ];
+   
 }
---
+++ storage/framework/views/e7c54d9cce31775987a10295c3f3a2a7.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <link rel="stylesheet" href="/css/create.css">
+
+    <title>Ride Secure</title>
+</head>
+<body>
+    <form action="/store" method="POST" enctype="multipart/form-data">
+     <?php echo csrf_field(); ?>
+     <input type="text" name="plate_number" placeholder="Plate Number" required>
+     <input type="text" name="model" placeholder="Model" required>
+     <input type="number" name="model_year" placeholder="Model Year" required>
+     <input type="text" name="chassis_number" placeholder="Chassis Number" required>
+     <input type="text" name="engine_number" placeholder="Engine Number" required>
+      <label for="" style="color: white" >Mulkiya Front Image</label> <input type="file" name="mulkiya_front_image" required>
+      <label for="" style="color: white" >Mulkiya Back Image</label><input type="file" name="mulkiya_back_image" required>
+     <button type="submit">Register Bike</button>
+     
+ </form>
+
+<?php if(session('msg')): ?>
+  <script>alert("<?php echo e(session('msg')); ?>");</script>
+<?php endif; ?>
+
+ <?php if($errors->any()): ?>
+    <script>
+        alert(`<?php echo implode('\n', $errors->all()); ?>`);
+    </script>
+<?php endif; ?>
+
+</body>
+</html><?php /**PATH C:\Laravel Code\Task\resources\views/Form.blade.php ENDPATH**/ ?>
