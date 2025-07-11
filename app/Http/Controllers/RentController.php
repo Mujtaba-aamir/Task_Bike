ad1be59679e0e1ccf9430abcbf9d45b98a271a9e
Mujtaba-aamir <ma7820464@gmail.com>
2025-07-11 06:33:33 +0500

    Added comments

--- app/Http/Controllers/RentController.php
+++ app/Http/Controllers/RentController.php
@@ -16,7 +16,7 @@ class RentController extends Controller
 
     public function calculateRent(Request $request)
     {
-        $request->validate([
+        $request->validate([  //validates input
             'bike_id' => 'required',
             'month' => 'required|date_format:Y-m',
             'monthly_rent' => 'required|integer|min:1',
@@ -29,18 +29,18 @@ class RentController extends Controller
         $daysInMonth = $month->daysInMonth;
         $dailyRate = round($monthly_rent / $daysInMonth, 2); 
 
-        $assignments = [];
+        $assignments = [];  //array to hold the data of each rider
 
         foreach ($bike->riders as $index => $rider) {
             $start = Carbon::parse($rider->pivot->assigned_at)->startOfDay();
-            $end = $rider->pivot->unassigned_at
-                ? Carbon::parse($rider->pivot->unassigned_at)->startOfDay()
-                : $endOfMonth;
+            $end = $rider->pivot->unassigned_at  
+                ? Carbon::parse($rider->pivot->unassigned_at)->startOfDay() //if the user is unassigned then that date of unassigned_at will be taken as $end
+                : $endOfMonth; //otherwise bike will be considered to be assigned for month
 
             $from = $start->lt($month) ? $month : $start;
             $to = $end->gt($endOfMonth) ? $endOfMonth : $end;
 
-            if ($to->lt($month) || $from->gt($endOfMonth)) continue;
+            if ($to->lt($month) || $from->gt($endOfMonth)) continue; //if assignment was done before that specific month or after then data of that rider will not be added in array 
 
             $assignments[] = [
                 'type' => 'rider',
@@ -50,13 +50,13 @@ class RentController extends Controller
             ];
         }
 
-        usort($assignments, fn($a, $b) => $a['from']->timestamp <=> $b['from']->timestamp);
+        usort($assignments, fn($a, $b) => $a['from']->timestamp <=> $b['from']->timestamp); //it sorts all the entries date wise by taking two entries from the array at a time
 
         $results = [];
         $currentDay = $month->copy();
 
         foreach ($assignments as $entry) {
-            if ($entry['from']->gt($currentDay)) {
+            if ($entry['from']->gt($currentDay)) { //if any gap occur in assigned dates of two consecutive assignments, then it will be considered company expense
                 $gapStart = $currentDay;
                 $gapEnd = $entry['from']->copy()->subDay();
                 $gapDays = $gapStart->diffInDays($gapEnd) + 1;
@@ -70,7 +70,7 @@ class RentController extends Controller
                 ];
             }
 
-            $riderDays = $entry['from']->diffInDays($entry['to']) + 1;
+            $riderDays = $entry['from']->diffInDays($entry['to']) + 1; //rent is calculated for the time bike was assigned
             $results[] = [
                 'rider' => $entry['name'],
                 'assigned_from' => $entry['from']->format('Y-m-d'),
@@ -82,7 +82,7 @@ class RentController extends Controller
             $currentDay = $entry['to']->copy()->addDay();
         }
 
-        if ($currentDay->lte($endOfMonth)) {
+        if ($currentDay->lte($endOfMonth)) { //after all entries of rider, if month is not completed then remaining days will be considered as company expense.
             $gapDays = $currentDay->diffInDays($endOfMonth) + 1;
             $results[] = [
                 'rider' => 'Company Expense',
